d663869747c70dcc23e8decb480a0238e3ff90c0
Yoojin Ivanovic <k.yoojin@gmail.com>
2013-05-21 21:59:30 -0700

    Adding a BufferedImageImpl class and some images

--- MeasureMe/src/com/measureme/core/BufferedImageImpl.java
+++ MeasureMe/src/com/measureme/core/BufferedImageImpl.java
@@ -1,43 +1,47 @@
 package com.measureme.core;
 
+import javax.imageio.ImageIo;
+import java.io.File;
+import java.awt.image.BufferedImage;
+
 public class BufferedImageImpl implements ImageInterface {
+
+	private String mFilename;
+	private BufferedImage mBufferedImage;
+
+	public BufferedImageImpl(String filename) {
+		mFilename = filename;
+		mBufferedImage = ImageIO.read(new File(filename));
+	}
+
+	@Override
+	public String getDebugName() {
+		return mFilename;
+	}
 
 	@Override
 	public int getPixel(int i, int j) {
-		// TODO Auto-generated method stub
-		return 0;
+		return mBufferedImage.getRGB(i, j);
 	}
 
 	@Override
 	public int getRed(int i, int j) {
-		// TODO Auto-generated method stub
-		return 0;
+		return (getPixel(i, j) >> 16) & 0xFF; 
 	}
 
 	@Override
 	public int getBlue(int i, int j) {
-		// TODO Auto-generated method stub
-		return 0;
+		return (getPixel(i, j)) & 0xFF;
 	}
 
 	@Override
 	public int getGreen(int i, int j) {
-		// TODO Auto-generated method stub
-		return 0;
+		return (getPixel(i, j) >> 8) & 0xFF;
 	}
 
 	@Override
 	public int getLuminance(int i, int j) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		return 0.2126 * getRed(i, j) + 0.7152 * getGreen(i, j) + 0.0722 * getBlue(i, j);
 	}
 
 }
